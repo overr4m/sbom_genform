b0b39b4697ca1d5ce70fb5158b25a577ab94ae65
maksim.vlasov <maksim.vlasov@vk.team>
2026-03-27 10:51:06 +0300

    feat: copyright auto fixies

--- scripts/update_copyright_year.py
+++ scripts/update_copyright_year.py
@@ -3,13 +3,13 @@ import re
 from datetime import datetime
 
 
-def update_copyright_year(directory, excluded_directories=None):
+def update_copyright_year(directory, excluded_dirs=None):
     # Получаем текущий и прошлый год
     current_year = datetime.now().year
     previous_year = current_year - 1
 
     # Регулярное выражение: ищем 'copyright', любой текст после него и прошлый год
-    # Например: "Copyright (c) 2025" -> "Copyright (c) 2026"
+    # Например: "Copyright (c) 2000" -> "Copyright (c) 2001"
     pattern = re.compile(rf'(copyright.*?){previous_year}', re.IGNORECASE)
     replacement = rf'\g<1>{current_year}'
 
@@ -32,16 +32,16 @@ def update_copyright_year(directory, excluded_directories=None):
                     new_content = pattern.sub(replacement, content)
                     with open(file_path, 'w', encoding='utf-8') as f:
                         f.write(new_content)
-                    print(f"✅ Обновлено: {file_path}")
+                    print(f"Обновлено: {file_path}")
 
             except (UnicodeDecodeError, PermissionError):
                 continue
             except Exception as e:
-                print(f"❌ Ошибка в {file_path}: {e}")
+                print(f"Ошибка в {file_path}: {e}")
 
 
 if __name__ == "__main__":
     # Укажите путь к папке (по умолчанию — текущая директория '.')
     target_directory = "."
-    excluded_directories = ["venv, tools"]
+    excluded_directories = ["venv", "tools", "__pycache__", "examples"]
     update_copyright_year(target_directory, excluded_directories)
